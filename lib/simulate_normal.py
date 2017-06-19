9a79a387d110fc6798bf8e34b126a483cb280f2f
Nick De Veaux <nrdeveaux@gmail.com>
2017-06-19 15:57:47 -0400

    write bam unimplemented error

--- lib/simulate_normal.py
+++ lib/simulate_normal.py
@@ -6,6 +6,20 @@ input_fasta_file = '../data/subsampled_hg38.fa'
 iterations = 100
 output_fasta_file = 'tests/output.fasta'
 
+def write_bam(mutated_genome):
+    raise NotImplementedError
+
+def write_fasta(mutated_genome):
+    # write fasta
+    output_seqs = []
+    for chrom in mutated_genome:
+        output_seq = SeqIO.SeqRecord(seq=mutated_genome[chrom].toseq(),
+              id=chrom, description='', name='')
+        output_seqs.append(output_seq)
+
+    with open(output_fasta_file, "w") as output_handle:
+        SeqIO.write(output_seqs, output_handle, "fasta")
+
 def main():
     # read genome fasta
     original_genome = {}
@@ -20,15 +34,8 @@ def main():
     for i in range(iterations):
         mutated_genome = orchestrator.snv(mutated_genome)
 
-    # write fasta
-    output_seqs = []
-    for chrom in mutated_genome:
-        output_seq = SeqIO.SeqRecord(seq=mutated_genome[chrom].toseq(),
-              id=chrom, description='', name='')
-        output_seqs.append(output_seq)
-
-    with open(output_fasta_file, "w") as output_handle:
-        SeqIO.write(output_seqs, output_handle, "fasta")
+    write_fasta(mutated_genome)
+    write_bam(mutated_genome)
 
 if __name__ == "__main__":
     main()
