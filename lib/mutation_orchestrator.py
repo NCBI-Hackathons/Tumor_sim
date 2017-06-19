34349258f3943e9192758608ea4ac856b46c070c
Nick De Veaux <nrdeveaux@gmail.com>
2017-06-19 14:06:54 -0400

    orchestrator

--- lib/mutation_orchestrator.py
+++ lib/mutation_orchestrator.py
@@ -3,3 +3,6 @@ from Bio import SeqIO
 class Mutation_Orchestrator:
     def deletion():
         return
+
+    def snv():
+        return
--- lib/tests/test_mutation_orchestrator.py
+++ lib/tests/test_mutation_orchestrator.py
@@ -6,7 +6,7 @@ from Bio.Alphabet import generic_dna
 class TestMutationOrchestrator(unittest.TestCase):
 
     def test_deletion(self):
-        my_seq = MutableSeq("ACTCGTCGTC", generic_dna)
-        mutation_creator = create_mutations.Mutation_Creator()
-        new_seq = mutation_creator.create_deletion(my_seq, start=5, end=10)
-        self.assertEqual(new_seq, MutableSeq("ACTCG", generic_dna))
+        return
+
+    def test_large_scale_inversion(self):
+        return
