819a0b585b9a68fbf911f736bce69c10201ebf64
Nick De Veaux <nrdeveaux@gmail.com>
2017-06-20 10:51:37 -0400

    translocations

--- lib/mutation_orchestrator.py
+++ lib/mutation_orchestrator.py
@@ -1,15 +1,17 @@
 from Bio import SeqIO
 import numpy as np
+from mutation_creator import Mutation_Creator
 
 class Mutation_Orchestrator:
     def __init__(self):
+        self.creator = Mutation_Creator()
         self.structural_variations = {
         'deletion': self.orchestrate_deletion,
         'translocation': self.orchestrate_translocation,
         'duplication' : self.orchestrate_duplication,
         'inversion' : self.orchestrate_inversion,
         'insertion' : self.orchestrate_insertion
-            }
+        }
     
         self.structural_variations_probabilities = {
         'deletion': 0.2,
@@ -17,32 +19,42 @@ class Mutation_Orchestrator:
         'duplication' : 0.2,
         'inversion' : 0.2,
         'insertion' : 0.2
-            }
+        }
 
     def deletion(self, genome):
         return genome
 
-    def snv(self, genome):
-        return genome
+    def snv_fast(self, genome, number):
+        
 
     def insertion(self, genome):
         return genome
 
-    def pick_chromosome(self, genome):
+    def pick_chromosomes(self, genome, number=1, replace=True):
         import pdb; pdb.set_trace()
         relative_lengths = np.array([len(genome[x]) for x in genome])
         probabilities = relative_lengths / float(relative_lengths.sum())
-        return np.random.choice(genome.keys(), 1, probabilities.tolist())
+        chroms = np.random.choice(genome.keys(), number, probabilities.tolist(), replace)
+        return chroms
 
     def get_location_on_sequence(seq, distribution='uniform'):
         if distribution == 'uniform':
             np.randint(len(seq))
 
     def orchestrate_deletion(genome, distribution='uniform'):
-        chrom = pick_chromosome(genome)
-        get_location_on_sequence()
+        chrom = self.pick_chromosome(genome)
+        start = self.get_location_on_sequence(genome[chrom])
+        # To be completed
 
     def orchestrate_translocation(genome, distribution='uniform'):
+        if len(genome) == 1:
+            print('No translocations allowed: genome too small')
+            return
+        (chrom_source, chrom_target) = self.pick_chromosomes(genome, number = 2, replace = False)
+        start_source = self.get_location_on_sequence(genome[chrom_source])
+        start_target = self.get_location_on_sequence(genome[chrom_target])
+        mutated_genome = creator
+
         print('in orchestrate_translocation')
 
     def orchestrate_duplication(genome,  distribution='uniform'):
