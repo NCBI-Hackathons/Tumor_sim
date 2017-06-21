dae5e4aa01c18c9cbd59975285a7cee6d5681ca2
Evan Biederstedt <evan.biederstedt@gmail.com>
2017-06-21 15:33:58 -0400

    update

--- lib/tests/test_simulate_normal.py
+++ lib/tests/test_simulate_normal.py
@@ -1,5 +1,5 @@
 import unittest
-from .. import simulate_normal
+from .. import simulate_endToEnd
 from Bio.Seq import MutableSeq
 from Bio.Alphabet import generic_dna
 
@@ -8,6 +8,6 @@ class TestSimulateNormal(unittest.TestCase):
     def test_remove_Ns(self):
         genome = {"chr1": MutableSeq("NNNNAGAGCTACGATGCTACGATGNNNNN", generic_dna),
                   "chr2": MutableSeq("NNNNAGAGCTACNNNGATGCGATGNN", generic_dna)}
-        genome_out = simulate_normal.remove_trailing_N_characters(genome)
+        genome_out = simulate_endToEnd.remove_trailing_N_characters(genome)
         self.assertEqual(genome_out, {"chr1": MutableSeq("AGAGCTACGATGCTACGATG", generic_dna),
                                       "chr2": MutableSeq("AGAGCTACNNNGATGCGATG", generic_dna)})
