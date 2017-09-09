c0ba6408898d5847aa2fc4f71ab5a1646a20c0a7
Evan Biederstedt <evan.biederstedt@gmail.com>
2017-09-09 11:39:03 -0400

    removed unncesseary Mutation_Creator objects

--- lib/mutation_orchestrator.py
+++ lib/mutation_orchestrator.py
@@ -188,11 +188,9 @@ class Mutation_Tracker:
 
         self.mutation_functions = {
         'deletion': self.creator.create_deletion,
-        'germline_deletion': self.creator.create_germline_deletion,
         'translocation': self.creator.create_translocation,
         'inversion' : self.creator.create_inversion,
-        'insertion' : self.creator.create_insertion,
-        'germline_insertion': self.creator.create_germline_insertion
+        'insertion' : self.creator.create_insertion
         }
 
 
