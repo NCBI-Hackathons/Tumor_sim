b4c9ba473ed9e06c5c02f963ca89db9ea50e322d
Evan Biederstedt <evan.biederstedt@gmail.com>
2017-11-01 14:32:20 -0400

    added normal distribution draws, for get_location_on_sequence()

--- lib/mutation_orchestrator.py
+++ lib/mutation_orchestrator.py
@@ -55,8 +55,16 @@ class Mutation_Orchestrator:
                 location = np.random.randint(len(seq))
                 if seq[location] != 'N':
                     return location
+        elif distribution == 'normal' or 'gaussian':
+            # Don't select a location with a N, and discard draws outside of bounds
+            while True:
+                mean = len(seq)/2
+                sigma = len(seq)/6
+                location =  np.random.normal(mean, sigma, 1)
+                if location < 0 or location > length(seq) or seq[location] != 'N':
+                    return location
         else:
-            raise NotImplementedError("Only Uniform is implemented!")
+            raise NotImplementedError("Only Uniform & Gaussian are implemented!")
 
     # Default to being a big deletion, but p=0.6 makes it a small deletion
     def orchestrate_deletion(self, genome, distribution='uniform', p=0.001):
